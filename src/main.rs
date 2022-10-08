8a0655201ac883babca3cebc269a2dfc5693edb6
Tobias Haueter <82332325+tobias-haueter@users.noreply.github.com>
2022-10-08 21:59:30 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,68 @@ use std::{io};
 use rand::Rng;
 use std::cmp::Ordering;
 use colored::*;
+use figlet_rs::FIGfont;
 
-fn main() {
+
+fn cli_figlet_rs(){
+    // https://crates.io/crates/figlet_rs
+    // use figlet_rs::FIGfont;
+
+    let standard_font = FIGfont::standard().unwrap();
+    let figure = standard_font.convert("Hello Rust");
+    assert!(figure.is_some());
+    println!("{}", figure.unwrap());
+
+}
+
+
+fn cli_guess_game(){
+
+    // figlet_rs
+    let standard_font = FIGfont::standard().unwrap();
+
+    //print title
+    let title = standard_font.convert("CLI_GG");
+    assert!(title.is_some());   
+    println!("{}", title.unwrap());
+    
     println!("{}", "------------------------------------------------------------------------".yellow());
-    println!("{}", "Guess the number! - RUST CLI GAME :-)".yellow());
+    println!("{}", "Guess the number! - RUST CLI GUESS GAME [rsCLI_GG])".yellow());
     println!("{}", "------------------------------------------------------------------------".yellow());
+    
+    // variables
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
     let mut count = 0;
+    let mut name = String::new();
+
+    println!("Hello, what's your name?");
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read name");
+
+    println!("{}", "------------------------------------------------------------------------".yellow());
+
+    let inputname = standard_font.convert(&name);
+    assert!(inputname.is_some());
+
+    println!("Hello, ");
+    println!("{}", inputname.unwrap());
+    println!("nice to have you here!");
+
+    println!("{}", "------------------------------------------------------------------------".yellow());
+
+    println!("OK, i have a secret number.... let's start the game!");
 
     loop {
         count += 1;
 
-        println!("Please input your guess #{count}:");
+        println!(
+            "{} {} {}",
+            "Please input your guess #".yellow(),
+            count.to_string().red(),
+            ":".yellow()
+        );
 
         
 
@@ -31,19 +80,25 @@ fn main() {
 
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}","Too small!".magenta().bold()),
+            Ordering::Less => println!("{}","Too small!".blue().bold()),
                             
             Ordering::Greater => println!("{}", "Too big!".red().bold()),
             
             Ordering::Equal => {
                 println!("{}", "------------------------------------------------------------------------".green());
-                println!("{}", "You win!".on_green().bold());
+                println!("{}", "You won!".on_green().bold());
                 println!("{}", "------------------------------------------------------------------------".green());
                 break;
             }
         }
     }
 
-    
+
+}
+
+fn main() {
+
+    cli_guess_game();
+    // cli_figlet_rs();
 
 }
